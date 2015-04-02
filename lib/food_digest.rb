7c1046cb03efe3f75d6b455a41bd5c8e364766cf
mdkalish <mdkalish4git@gmail.com>
2015-04-02 12:00:30 +0200

    Setup rspec

--- lib/food_digest.rb
+++ lib/food_digest.rb
@@ -1,3 +1,4 @@
+require 'food_digest/authorization'
 module FoodDigest
 end
 
---
+++ spec/lib/authorization_spec.rb
@@ -0,0 +1,8 @@
+describe FoodDigest::Authorization do
+  context 'test context' do
+    it 'test it' do
+      expect(2).to eq(2)
+    end
+  end
+end
+
